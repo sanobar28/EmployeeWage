d44e7af56dcb1a8d23a0d69970c0a00592f77700
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-06 16:51:59 +0530

    added: Method to check present or absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,7 +5,22 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee wage computation program");
-
+		
+		checkPresent();
 	}
 
+	/**
+	 * UC-1 to check if employee is present or absent
+	 */
+	private static void checkPresent() {
+		
+		double empCheck = Math.random();
+		if (empCheck > 0.5) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+		
+	}
 }
