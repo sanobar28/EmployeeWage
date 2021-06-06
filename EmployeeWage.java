86633fb8a81c7f21ac685af20980e66920213a4f
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-06 19:29:05 +0530

    Refactor: calculate total wage using class method & variable

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,26 +6,33 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORK_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
+	
+	static int empHrs=0;
+	static int totalEmpWage = 0;
+	static int totalWorkingHrs=0; 
+	static int totalWorkdays=0;
+	
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee wage computation program");
 		
-		checkPresent();
 		
+		calculateWage();
 	}
 
 	/**
-	 * UC-1 to check if employee is present or absent
-	 * UC-3 to check if Employee is part time and calculate Wage
-	 * UC-4 to calculate Wage using switch statements 
+	 * UC-6 Code refactor and use class method and class variable
 	 */
-	private static int checkPresent() {
+	private static void calculateWage() {
 		
-		int empHrs=0, totalEmpWage = 0;
-		int empcheck = (int) Math.floor(Math.random()*10)%3;
+
+		while (totalWorkingHrs <= MAX_HRS_IN_MONTH && 
+				totalWorkdays < NUM_OF_WORK_DAYS){
 			
-		for (int day=1; day <= NUM_OF_WORK_DAYS; day++){
+			totalWorkdays++;
+			int empcheck = (int) Math.floor(Math.random()*10)%3;
 			switch (empcheck){
 				
 				case(IS_FULL_TIME):
@@ -39,22 +46,13 @@ public class EmployeeWage {
 				default:
 					empHrs=0;
 			}
-			EmpWage(empHrs,totalEmpWage);
+			totalWorkingHrs += empHrs;
+			int empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
 		}
-		
-		return totalEmpWage;
-		
-	}
 	
-	/**
-	 * UC-2 to calculate Employee wage per day
-	 * UC-5 to calculate Employee wage per month
-	 * @param empHrs
-	 */
-	private static void EmpWage(int empHrs, int totalEmpWage) {
-		
-		int empWage = empHrs * EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage;
-		
-	}
+		System.out.println("Total working hours : " + totalWorkingHrs);
+		System.out.println("No. of working days: " + totalWorkdays);
+		System.out.println("Total Emp Wage for month: " + totalEmpWage);
+	}	
 }
