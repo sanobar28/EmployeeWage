33cb9891643361ab33737b8f8e9b987fd73592fc
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-08 19:04:17 +0530

    added: array used to store different company wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,3 +1,13 @@
+/**
+ * 
+ * @author Sanobar Mujawar
+ * Created_On: 08.06.21
+ * 
+ * Purpose: To calculate Employee wage given by different companies
+ * 
+ */
+
+
 package com;
 
 public class EmployeeWage {
@@ -5,33 +15,41 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
-	private final String company;
-	private final int empRatePerHour;
-	private final int numOfWorkingDays;
-	private final int maxHoursPerMonth;
-	private int totalEmpWage;
-	
-	/**
-	 * UC-8 Instance variable created using class constructor
-	 * 
-	 * @param company
-	 * @param empRatePerHour
-	 * @param numOfWorkingDays
-	 * @param maxHoursPerMonth
-	 */
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWage;
 
-	public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		this.company = company;
-		this.empRatePerHour = empRatePerHour;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursPerMonth = maxHoursPerMonth;
+	/**
+	 * UC-10 array of CompanyEmpWage object
+	 * 
+	 */
+	public EmployeeWage() {
+		// array for 5 companies
+		companyEmpWage = new CompanyEmpWage[5];
 	}
 
-	public void computeEmpWage() {
+	private void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		companyEmpWage[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+		numOfCompany++;
+	}
+
+	private void computeEmpWage() {
+		for (int i = 0; i < numOfCompany; i++) {
+			companyEmpWage[i].setTotalEmpWage(this.computeEmpWage(companyEmpWage[i]));
+			System.out.println(companyEmpWage[i]);
+		}
+	}
+	
+	/**
+	 * Method to compute Employee wage
+	 * @param companyEmpWage
+	 * @return
+	 */
+
+	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
 		int empHrs = 0;
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;
-		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+		while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -51,28 +69,50 @@ public class EmployeeWage {
 			System.out.println("Day : " + totalWorkingDays + "Emp Hr: " + empHrs);
 
 		}
-		totalEmpWage = totalEmpHrs * empRatePerHour;
+		return totalEmpHrs * companyEmpWage.empRatePerHour;
 	}
 
-	@Override
-	public String toString() {
-		return "Total Emp Wage for Company : " + company + " is: " + totalEmpWage;
+	/**
+	 * main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EmployeeWage EmployeeWage = new EmployeeWage();
+		EmployeeWage.addCompanyEmpWage("Dmart", 20, 2, 10);
+		EmployeeWage.addCompanyEmpWage("Reliance", 10, 4, 20);
+		EmployeeWage.computeEmpWage();
+
 	}
 	
 	/**
-	 * class objects are created by passing parameters to class constructor
-	 * and computed wage for different companies
-	 * @param args
+	 * 
+	 * Class ComapnyEmpWage with constructor and instance variables
+	 *
 	 */
-	
-	public static void main(String[] args) {
-		EmployeeWage dMart = new EmployeeWage("Dmart", 20, 2, 10);
-		EmployeeWage relience = new EmployeeWage("Reliance", 10, 4, 20);
-		dMart.computeEmpWage();
-		System.out.println(dMart);
-		relience.computeEmpWage();
-		System.out.println(relience);
 
+	public class CompanyEmpWage {
+
+		public final String company;
+		public final int empRatePerHour;
+		public final int numOfWorkingDays;
+		public final int maxHoursPerMonth;
+		public int totalEmpWage;
+
+		public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+			this.company = company;
+			this.empRatePerHour = empRatePerHour;
+			this.numOfWorkingDays = numOfWorkingDays;
+			this.maxHoursPerMonth = maxHoursPerMonth;
+		}
+
+		public void setTotalEmpWage(int totalEmpWage) {
+			this.totalEmpWage = totalEmpWage;
+		}
+
+		@Override
+		public String toString() {
+			return "Total Emp Wage for Company : " + company + " is: " + totalEmpWage;
+		}
 	}
 
 }
